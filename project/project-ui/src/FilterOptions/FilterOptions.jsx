ec10ada48109973fae7ff900ad68fbe7355a0f65
Ana Camba Gomes <121646377+anacambag@users.noreply.github.com>
2023-07-20 10:42:17 -0700

    filterOptions working with multiple selections and creates and object and the time range is single-selection (#31)
    Co-authored-by: Ana Camba Gomes <acamba@acamba-ltmya7c.internal.salesforce.com>

--- project/project-ui/src/FilterOptions/FilterOptions.jsx
+++ project/project-ui/src/FilterOptions/FilterOptions.jsx
@@ -3,290 +3,185 @@ import "./FilterOptions.css";
 import { useState } from "react";
 
 export default function FilterOptions() {
-  const [selectedOption, setSelectedOption] = useState("");
-  const [selectedSubOption, setSelectedSubOption] = useState("");
-  const [showDietaryOptions, setShowDietaryOptions] = useState(false);
-  const [showCusines, setShowCusines] = useState(false);
-  const [showMealTypes, setShowMealTypes] = useState(false);
-  const [showTimeRange, setShowTimeRange] = useState(false);
-  const [selectedSubOptionsList, setSelectedSubOptionsList] = useState([]);
 
+        const [selectedOption, setSelectedOption] = useState("");
+        const [selectedSubOption, setSelectedSubOption] = useState("");
+        const [showDietaryOptions, setShowDietaryOptions] = useState(false);
+        const [showCusines, setShowCusines] = useState(false);
+        const [showMealTypes, setShowMealTypes] = useState(false);
+        const [showTimeRange, setShowTimeRange] = useState(false);
+      
+        const [filterState, setFilterState] = useState({
+          maxReadyTime: "",
+          selectedSubOptions: {
+            dietary: [],
+            cuisine: [],
+            meal: [],
+          },
+        });
+      
+        const handleOptionClick = (option) => {
+          setSelectedOption(option);
 
+          // Toggle the state for each category
+          setShowDietaryOptions(option === "dietary");
+          setShowCusines(option === "cuisine");
+          setShowMealTypes(option === "meal");
+          setShowTimeRange(option === "time");
+        };
+      
+        const handleSubOptionClick = (subOption) => {
+            const { selectedSubOptions } = filterState;
+            const isActive =
+              selectedOption !== "time" && selectedSubOptions[selectedOption].includes(subOption);
+          
+              if (selectedOption === "time") {
+                // For the "time" category, toggle the selectedSubOption
+                setSelectedSubOption((prevSubOption) => (prevSubOption === subOption ? "" : subOption));
+          
+                // Update maxReadyTime based on the selectedSubOption
+                setFilterState((f) => ({
+                  ...f,
+                  maxReadyTime: selectedSubOption === subOption ? "" : subOption,
+                }));
+              } else {
+              // For other categories, toggle the active state of the subOption
+              const updatedSelectedSubOptions = isActive
+                ? selectedSubOptions[selectedOption].filter((item) => item !== subOption)
+                : [...selectedSubOptions[selectedOption], subOption];
+          
+              setFilterState((f) => ({
+                ...f,
+                selectedSubOptions: {
+                  ...f.selectedSubOptions,
+                  [selectedOption]: updatedSelectedSubOptions,
+                },
+              }));
+            }
+          };
+
+  // Helper function to convert time option to minutes
+//   const getMaxReadyTimeValue = (subOption) => {
+//     switch (subOption) {
+//       case "< 15 mins":
+//         return 15;
+//       case "< 30 mins":
+//         return 30;
+//       case "< 45 mins":
+//         return 45;
+//       case "< 1 hr":
+//         return 60;
+//       case "< 2 hr":
+//         return 120;
+//       default:
+//         return "";
+//     }
+//   };
   
-
-  const [filterState, setFilterState] = useState({
-    cuisine: "",
-    diet: "",
-    type: "",
-    maxReadyTime: ""
-  })
-
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setSelectedSubOption(""); // Reset selected suboption when switching categories
-
-    // Toggle the state for each category
-    setShowDietaryOptions(option === "dietary");
-    setShowCusines(option === "cuisine");
-    setShowMealTypes(option === "meal");
-    setShowTimeRange(option === "time");
-  };
-
-  const handleSubOptionClick = (subOption) => {
-
-    setSelectedSubOption(subOption);
-    if (selectedOption === "dietary"){
-        setFilterState((f) => ({ ...f, diet: subOption}));
-    }  
-    if (selectedOption === "cuisine"){
-        setFilterState((f) => ({ ...f, cuisine: subOption}));
-    } 
-    if (selectedOption === "meal"){
-        setFilterState((f) => ({ ...f, type: subOption}));
-    }
-    if (selectedOption === "time"){
-        if(subOption === "< 15 mins")
-        setFilterState((f) => ({ ...f, maxReadyTime: 15}))
-
-        if(subOption === "< 30 mins")
-        setFilterState((f) => ({ ...f, maxReadyTime: 30}))
-
-        if(subOption === "< 45 mins")
-        setFilterState((f) => ({ ...f, maxReadyTime: 45}))
-
-        if(subOption === "< 1 hr")
-        setFilterState((f) => ({ ...f, maxReadyTime: 60}))
-
-        if(subOption === "< 2 hr")
-        setFilterState((f) => ({ ...f, maxReadyTime: 120}))
-    } 
-    // setSelectedSubOptionsList((prevList) => {
-    //     if (!prevList.includes(subOption)) {
-    //       return [...prevList, subOption];
-    //     }
-    //     return prevList;
-    //   });
-     
-  };
 
   console.log(filterState)
   
 
+  const options = {
+    dietary: ["Vegan", "Kosher", "Halal", "Gluten-free", "Vegetarian"],
+    cuisine: [
+      "Italian",
+      "Greek",
+      "Spanish",
+      "Chinese",
+      "Japanese",
+      "Indian",
+      "Mexican",
+      "Turkish",
+      "American",
+      "French",
+      "Latin American",
+    ],
+    meal: ["Breakfast", "Main Course", "Dessert", "Appetizer", "Snack"],
+    time: [
+      { label: "< 15 mins", value: 15 },
+      { label: "< 30 mins", value: 30 },
+      { label: "< 45 mins", value: 45 },
+      { label: "< 1 hr", value: 60 },
+      { label: "< 2 hr", value: 120 },
+    ],
+  };
+
   return (
     <>
       <div className="SubNavbar">
-        <span
-          className={selectedOption === "dietary" ? "active" : ""}
-          onClick={() => handleOptionClick("dietary")}
-        >
-          Dietary Restrictions
-        </span>
-        <span
-          className={selectedOption === "meal" ? "active" : ""}
-          onClick={() => handleOptionClick("meal")}
-        >
-          Meal Types
-        </span>
-        <span
-          className={selectedOption === "cuisine" ? "active" : ""}
-          onClick={() => handleOptionClick("cuisine")}
-        >
-          Cuisine
-        </span>
-        <span
-          className={selectedOption === "time" ? "active" : ""}
-          onClick={() => handleOptionClick("time")}
-        >
-          Time Range
-        </span>
+        {Object.keys(options).map((option) => (
+          <span
+            key={option}
+            className={selectedOption === option ? "active" : ""}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option === "time" ? "Time Range" : option.charAt(0).toUpperCase() + option.slice(1)}
+          </span>
+        ))}
       </div>
       <div className="subnavbar-options">
-        {showDietaryOptions && (
-          <div className="DietaryOptions">
+        {showDietaryOptions &&
+          options.dietary.map((subOption) => (
             <button
-              className={selectedSubOption === "Vegan" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Vegan")}
+              key={subOption}
+              className={filterState.selectedSubOptions["dietary"].includes(subOption) ? "active" : ""}
+              onClick={() => handleSubOptionClick(subOption)}
             >
-              Vegan
+              {subOption}
             </button>
+          ))}
+        {showCusines &&
+          options.cuisine.map((subOption) => (
             <button
-              className={selectedSubOption === "Kosher" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Kosher")}
+              key={subOption}
+              className={filterState.selectedSubOptions["cuisine"].includes(subOption) ? "active" : ""}
+              onClick={() => handleSubOptionClick(subOption)}
             >
-              Kosher
+              {subOption}
             </button>
+          ))}
+        {showMealTypes &&
+          options.meal.map((subOption) => (
             <button
-              className={selectedSubOption === "Halal" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Halal")}
+              key={subOption}
+              className={filterState.selectedSubOptions["meal"].includes(subOption) ? "active" : ""}
+              onClick={() => handleSubOptionClick(subOption)}
             >
-              Halal
+              {subOption}
             </button>
+          ))}
+        {showTimeRange &&
+          options.time.map((subOption) => (
             <button
-              className={selectedSubOption === "Gluten-free" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Gluten-free")}
+              key={subOption.value}
+              className={selectedSubOption === subOption.value ? "active" : ""}
+              onClick={() => handleSubOptionClick(subOption.value)}
             >
-              Gluten-free
+              {subOption.label}
             </button>
-            <button
-              className={selectedSubOption === "Vegetarian" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Vegetarian")}
-            >
-              Vegetarian
-            </button>
-          </div>
-        )}
-        {showCusines && (
-          <div className="DietaryOptions">
-            <button
-              className={selectedSubOption === "Italian" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Italian")}
-            >
-              Italian
-            </button>
-            <button
-              className={selectedSubOption === "Greek" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Greek")}
-            >
-              Greek
-            </button>
-            <button
-              className={selectedSubOption === "Spanish" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Spanish")}
-            >
-              Spanish
-            </button>
-            <button
-              className={selectedSubOption === "Chinese" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Chinese")}
-            >
-              Chinese
-            </button>
-            <button
-              className={selectedSubOption === "Japanese" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Japanese")}
-            >
-              Japanese
-            </button>
-            <button
-              className={selectedSubOption === "Indian" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Indian")}
-            >
-              Indian
-            </button>
-            <button
-              className={selectedSubOption === "Mexican" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Mexican")}
-            >
-              Mexican
-            </button>
-            <button
-              className={selectedSubOption === "Turkish" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Turkish")}
-            >
-              Turkish
-            </button>
-            <button
-              className={selectedSubOption === "American" ? "active" : ""}
-              onClick={() => handleSubOptionClick("American")}
-            >
-              American
-            </button>
-            <button
-              className={selectedSubOption === "French" ? "active" : ""}
-              onClick={() => handleSubOptionClick("French")}
-            >
-              French
-            </button>
-            <button
-              className={selectedSubOption === "Latin American" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Latin American")}
-            >
-              Latin American
-            </button>
-          </div>
-        )}
+          ))}
+      </div>
+      <div className="selected-options">
+        <h3>Selected Filters:</h3>
+        {Object.keys(filterState.selectedSubOptions).map((category) => {
+          if (category !== "time") {
+            return filterState.selectedSubOptions[category].map((subOption) => (
+              <button key={subOption} className="selected-options-button">
+                {subOption}
+              </button>
+            ));
+          }
+          return null; // Skip rendering the "time" category
+        })}
 
-        {showMealTypes && (
-          <div className="DietaryOptions">
-            <button
-              className={selectedSubOption === "Breakfast" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Breakfast")}
-            >
-              Breakfast
-            </button>
-            <button
-              className={selectedSubOption === "Main Course" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Main Course")}
-            >
-              Main Course
-            </button>
-            <button
-              className={selectedSubOption === "Dessert" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Dessert")}
-            >
-              Dessert
-            </button>
-            <button
-              className={selectedSubOption === "Appetizer" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Appetizer")}
-            >
-              Appetizer
-            </button>
-            <button
-              className={selectedSubOption === "Snack" ? "active" : ""}
-              onClick={() => handleSubOptionClick("Snack")}
-            >
-              Snack
-            </button>
-          </div>
+        {/* Display maxReadyTime if it has a value */}
+        {filterState.maxReadyTime && (
+          <button className="selected-options-button">{filterState.maxReadyTime}</button>
         )}
-        {showTimeRange && (
-          <div className="DietaryOptions">
-            <button
-              className={selectedSubOption === "< 15 mins" ? "active" : ""}
-              onClick={() => handleSubOptionClick("< 15 mins")}
-            >
-              &lt; 15 mins
-            </button>
-            <button
-              className={selectedSubOption === "< 30 mins" ? "active" : ""}
-              onClick={() => handleSubOptionClick("< 30 mins")}
-            >
-              &lt; 30 mins
-            </button>
-            <button
-              className={selectedSubOption === "< 45 mins" ? "active" : ""}
-              onClick={() => handleSubOptionClick("< 45 mins")}
-            >
-              &lt; 45 mins
-            </button>
-            <button
-              className={selectedSubOption === "< 1 hr" ? "active" : ""}
-              onClick={() => handleSubOptionClick("< 1 hr")}
-            >
-              &lt; 1 hr
-            </button>
-            <button
-              className={selectedSubOption === "< 2 hr" ? "active" : ""}
-              onClick={() => handleSubOptionClick("< 2 hr")}
-            >
-              &lt; 2 hr
-            </button> 
-          </div>
-        )}
-        <div className="selected-options">
-  {Object.keys(filterState)?.map((key) => {
-    if (filterState[key] !== "") {
-      return (
-        <button key={key} className="active">
-          {filterState[key]}
-        </button>
-      );
-    }
-    return null;
-  })}
-</div>
       </div>
     </>
   );
 }
+
+
