2336e48212cc6e2414dffbccbb2e760d36cdcf21
Nara Macias <116755949+Lilnnara@users.noreply.github.com>
2023-07-25 16:41:50 -0700

    added loaders to grids (#46)
    Co-authored-by: Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>

--- project/project-ui/src/FavoriteRecipesGrid/FavoriteRecipesGrid.jsx
+++ project/project-ui/src/FavoriteRecipesGrid/FavoriteRecipesGrid.jsx
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./FavoriteRecipesGrid.css";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import apiClient from "../../services/apiClient";
+import RecipeCardLoader from "../Loaders/RecipeCardLoader";
 
 export default function FavoriteRecipesGrid({ user }) {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchRecipes = async () => {
+      setLoading(true); // Start loading
+
       // Fetch favorite recipe IDs
       const favoriteRes = await apiClient.getFavoriteRecipes(user.id);
       if (favoriteRes.error) {
         console.error("Error fetching favorite recipe IDs:", favoriteRes.error);
+        setLoading(false); // End loading if there was an error
         return;
       }
 
@@ -23,10 +28,12 @@ export default function FavoriteRecipesGrid({ user }) {
       const recipeRes = await apiClient.getBulkRecipeInformation(favoriteIds);
       if (recipeRes.error) {
         console.error("Error fetching favorite recipes:", recipeRes.error);
+        setLoading(false); // End loading if there was an error
         return;
       }
 
       setRecipes(recipeRes.data);
+      setLoading(false); // End loading
     };
 
     fetchRecipes();
@@ -35,9 +42,13 @@ export default function FavoriteRecipesGrid({ user }) {
   return (
     <>
       <div className="favorite-recipes-grid">
-        {recipes.map((recipe) => (
-          <RecipeCard user={user} recipe={recipe} key={recipe.id} />
-        ))}
+        {loading
+          ? Array(12)
+              .fill()
+              .map((_, index) => <RecipeCardLoader key={index} />)
+          : recipes.map((recipe) => (
+              <RecipeCard user={user} recipe={recipe} key={recipe.id} />
+            ))}
       </div>
     </>
   );
