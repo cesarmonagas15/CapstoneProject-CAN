fc89fc32cf45ab8b5644deea565319a987445828
Nara Macias <116755949+Lilnnara@users.noreply.github.com>
2023-07-27 11:39:16 -0700

    loader animation added for recipe details (#50)
    Co-authored-by: Nara Macias <nmacias@nmacias-ltmujk4.internal.salesforce.com>

---
+++ project/project-ui/src/Loaders/RecipeDetailsLoader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const RecipeDetailsLoader = (props) => (
+  <ContentLoader
+    width={1500}
+    height={800}
+    viewBox="0 0 800 600"
+    backgroundColor="#f0f0f0"
+    foregroundColor="#dedede"
+    {...props}
+  >
+    <rect x="270" y="20" rx="5" ry="5" width="500" height="350" /> // Image
+    <rect x="365" y="400" rx="4" ry="4" width="300" height="20" /> // Title
+    <rect x="50" y="460" rx="4" ry="4" width="500" height="10" /> // Description
+    line 1
+    <rect x="50" y="480" rx="4" ry="4" width="500" height="10" /> // Description
+    line 2
+    <rect x="50" y="500" rx="4" ry="4" width="500" height="10" /> // Description
+    line 3
+    <rect x="50" y="520" rx="4" ry="4" width="500" height="10" /> // Description
+    line 4
+  </ContentLoader>
+);
+
+export default RecipeDetailsLoader;
--- project/project-ui/src/RecipeDetails/RecipeDetails.jsx
+++ project/project-ui/src/RecipeDetails/RecipeDetails.jsx
@@ -1,65 +1,71 @@
-import * as React from "react"
-import "./RecipeDetails.css"
-import axios from "axios"
-import { useParams } from "react-router-dom"
-import { useNavigate } from 'react-router-dom';
-import {useState, useEffect} from 'react'
+import * as React from "react";
+import "./RecipeDetails.css";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import apiClient from "../../services/apiClient";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import RecipeNutritionFacts from "../RecipeNutritionFacts/RecipeNutritionFacts";
-
-
+import RecipeDetailsLoader from "../Loaders/RecipeDetailsLoader";
 
 export default function RecipeDetails() {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const {id} = useParams();
-    const [recipe, setRecipe] = useState({});
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      setLoading(true); // Begin loading
+      const { data, error } = await apiClient.getRecipeInformation(id); // recipe id
+      if (data) setRecipe(data);
+      if (error) {
+        console.error("Error fetching recipe:", error);
+        setLoading(false); // End loading if there was an error
+      }
+      setLoading(false); // End loading after we set the recipe
+    };
+    fetchRecipe();
+  }, []);
 
+  const navigate = useNavigate();
+  const [checkedIngredients, setCheckedIngredients] = useState({});
+  const clockIcon = <FontAwesomeIcon icon={faClock} />;
+  const userIcon = <FontAwesomeIcon icon={faUser} />;
+  const thumbsUp = <FontAwesomeIcon icon={faThumbsUp} />;
+  const convertHtmlToText = (html) => {
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = html;
+    return tempElement.textContent || tempElement.innerText || "";
+  };
+  const formattedSummary = convertHtmlToText(recipe.summary).replace(
+    /\.\s/g,
+    ".\n\n"
+  );
 
-    useEffect(()=> {
-        const fetchRecipe = async () => {
-            const {data, error}= await apiClient.getRecipeInformation(id); // recipe id
-            if (data) setRecipe(data);
-            if (error) console.error("Error fetching recipe:", error);
-          };
-          fetchRecipe();
-    },[]);
+  const handleIngredientCheck = (ingredientId) => {
+    setCheckedIngredients((prevState) => ({
+      ...prevState,
+      [ingredientId]: !prevState[ingredientId],
+    }));
+  };
 
-      const navigate = useNavigate();
-      const [checkedIngredients, setCheckedIngredients] = useState({});
-      const clockIcon = <FontAwesomeIcon icon={faClock} />;
-      const userIcon = <FontAwesomeIcon icon={faUser} />;
-      const thumbsUp = <FontAwesomeIcon icon={faThumbsUp} />;
-      const convertHtmlToText = (html) => {
-        const tempElement = document.createElement('div');
-        tempElement.innerHTML = html;
-        return tempElement.textContent || tempElement.innerText || '';
-      };
-      const formattedSummary = convertHtmlToText(recipe.summary).replace(/\.\s/g, '.\n\n');
-  
-      const handleIngredientCheck = (ingredientId) => {
-          setCheckedIngredients((prevState) => ({
-            ...prevState,
-            [ingredientId]: !prevState[ingredientId],
-          }));
-        };
+  const [doneInstructions, setDoneInstructions] = useState({});
+  const handleInstructionClick = (index) => {
+    // Create a new copy of the doneInstructions object to avoid mutation
+    const newDoneInstructions = { ...doneInstructions };
+    // Toggle the "done" state for the clicked instruction index
+    newDoneInstructions[index] = !newDoneInstructions[index];
+    // Update the state with the new "done" instructions
+    setDoneInstructions(newDoneInstructions);
+  };
+  const percentCarbs = recipe?.nutrition?.caloricBreakdown?.percentCarbs || "";
+  const percentFat = recipe?.nutrition?.caloricBreakdown?.percentFat || "";
+  const percentProtein =
+    recipe?.nutrition?.caloricBreakdown?.percentProtein || "";
 
-        const [doneInstructions, setDoneInstructions] = useState({});
-        const handleInstructionClick = (index) => {
-          // Create a new copy of the doneInstructions object to avoid mutation
-          const newDoneInstructions = { ...doneInstructions }
-          // Toggle the "done" state for the clicked instruction index
-          newDoneInstructions[index] = !newDoneInstructions[index];
-          // Update the state with the new "done" instructions
-          setDoneInstructions(newDoneInstructions);
-        };
-          const percentCarbs = recipe?.nutrition?.caloricBreakdown?.percentCarbs || '';
-          const percentFat = recipe?.nutrition?.caloricBreakdown?.percentFat || '';
-          const percentProtein = recipe?.nutrition?.caloricBreakdown?.percentProtein || '';
-        
   return (
     <>
       <button className="back-button" onClick={() => navigate(-1)}>
@@ -71,82 +77,91 @@ export default function RecipeDetails() {
           <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
         </svg>
       </button>
+      {loading ? (
+        <RecipeDetailsLoader />
+      ) : (
+        <div className="recipe-details-container">
+          <div className="recipe-image">
+            <img src={recipe.image} alt={recipe.title} />
+          </div>
+          <h1 className="recipe-details-recipe-title">{recipe.title}</h1>
+          <hr className="recipe-title-divider"></hr>
 
-      <div className="recipe-details-container">
-        <div className="recipe-image">
-          <img src={recipe.image} alt={recipe.title} />
-        </div>
-        <h1 className="recipe-details-recipe-title">{recipe.title}</h1>
-        <hr className="recipe-title-divider"></hr>
+          <div className="recipe-details-columns">
+            <div className="recipe-details-col">
+              <div className="recipe-summary-container">
+                <p className="recipe-summary">{formattedSummary}</p>
+              </div>
 
-        <div className="recipe-details-columns">
-          <div className="recipe-details-col">
-            <div className="recipe-summary-container">
-              <p className="recipe-summary">{formattedSummary}</p>
-            </div>
-
-            <div className="serving-info">
-              <ul className="serving-details">
-                <li className="details-item">
-                  <div className="item-content">
-                    {clockIcon}
-                    <span className="details-value">{recipe.readyInMinutes}</span>
-                  </div>
-                  <span className="title">Minutes</span>
-                </li>
-                <li className="details-item">
-                  <div className="item-content">
-                    {userIcon}
-                    <span className="details-value">{recipe.servings}</span>
-                  </div>
-                  <span className="title">Servings</span>
-                </li>
-                { recipe.aggregateLikes & recipe.aggregateLikes > 1 ?
-                <li className="details-item">
-                <div className="item-content">
-                  {thumbsUp}
-                  <span className="details-value">{recipe.aggregateLikes}</span>
-                  </div>
-                  <span className="title">Likes</span>
-                </li>
-                :
-                <></>
-                }
-              </ul>
-            </div>
-            <div className="recipe-details-container-ingredients">
-              <h3>Ingredients:</h3>
-              <ul className="ingredients-box">
-                {recipe.extendedIngredients?.map((ingredient) => (
-                  <li key={ingredient.id}
-                      className={checkedIngredients[ingredient.id] ? "checked" : ""}
-                  >
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={checkedIngredients[ingredient.id] || false}
-                        onChange={() => handleIngredientCheck(ingredient.id)}
-                      />
-                      {ingredient.original}
-                    </label>
+              <div className="serving-info">
+                <ul className="serving-details">
+                  <li className="details-item">
+                    <div className="item-content">
+                      {clockIcon}
+                      <span className="details-value">
+                        {recipe.readyInMinutes}
+                      </span>
+                    </div>
+                    <span className="title">Minutes</span>
                   </li>
-                ))}
-              </ul>
+                  <li className="details-item">
+                    <div className="item-content">
+                      {userIcon}
+                      <span className="details-value">{recipe.servings}</span>
+                    </div>
+                    <span className="title">Servings</span>
+                  </li>
+                  {recipe.aggregateLikes & (recipe.aggregateLikes > 1) ? (
+                    <li className="details-item">
+                      <div className="item-content">
+                        {thumbsUp}
+                        <span className="details-value">
+                          {recipe.aggregateLikes}
+                        </span>
+                      </div>
+                      <span className="title">Likes</span>
+                    </li>
+                  ) : (
+                    <></>
+                  )}
+                </ul>
+              </div>
+              <div className="recipe-details-container-ingredients">
+                <h3>Ingredients:</h3>
+                <ul className="ingredients-box">
+                  {recipe.extendedIngredients?.map((ingredient) => (
+                    <li
+                      key={ingredient.id}
+                      className={
+                        checkedIngredients[ingredient.id] ? "checked" : ""
+                      }
+                    >
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={checkedIngredients[ingredient.id] || false}
+                          onChange={() => handleIngredientCheck(ingredient.id)}
+                        />
+                        {ingredient.original}
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div className="recipe-details-col-nutrution-facts">
-            <div className="recipe-details-nutrition-facts-container">
-            <RecipeNutritionFacts 
-              percentCarbs = {percentCarbs}
-              percentFat = {percentFat}
-              percentProtein = {percentProtein}
-               />
+            <div className="recipe-details-col-nutrution-facts">
+              <div className="recipe-details-nutrition-facts-container">
+                <RecipeNutritionFacts
+                  percentCarbs={percentCarbs}
+                  percentFat={percentFat}
+                  percentProtein={percentProtein}
+                />
+              </div>
             </div>
           </div>
-        </div>
-                <div className="instructions-container">
-        {recipe.analyzedInstructions ? (
+          <div className="instructions-container">
+            {recipe.analyzedInstructions ? (
               <div className="instructions">
                 <h3>Instructions:</h3>
                 <ul className="instruction-list">
@@ -167,21 +182,23 @@ export default function RecipeDetails() {
             ) : (
               <></>
             )}
-            </div>
-            { recipe.sourceUrl? (
+          </div>
+          {recipe.sourceUrl ? (
             <div className="view-full-container">
-              <button 
+              <button
                 className="view-full-button"
-                onClick={() => { window.open(recipe.sourceUrl, '_blank')}}>
+                onClick={() => {
+                  window.open(recipe.sourceUrl, "_blank");
+                }}
+              >
                 View Full Recipe
               </button>
             </div>
-            )
-            :
+          ) : (
             <></>
-              }
-
-      </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
