a39ab1bdafd738f08ce606915d915208529f44f4
Cesar Monanagas Romero <121134903+cesarmonagas15@users.noreply.github.com>
2023-08-07 11:34:16 -0700

    footer links changes (#88)
    Co-authored-by: Cesar Monagas Romero <cmonagasromero@cmonaga-ltmyrcq.internal.salesforce.com>

--- project/project-ui/src/Footer/Footer.jsx
+++ project/project-ui/src/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import Footer from "rc-footer";
 import "rc-footer/assets/index.css"; // import 'rc-footer/asssets/index.less';
 import React from "react";
+import "./Footer.css"
 
 export default function CustomFooter() {
   const year = new Date().getFullYear();
@@ -11,7 +12,9 @@ export default function CustomFooter() {
   );
 
   return (
-    <div>
+    <>
+    <br/>
+    <div className="footer-container">
     <Footer
         columns={[
           {
@@ -29,12 +32,12 @@ export default function CustomFooter() {
             items: [
               {
                 title: 'Mission',
-                url: 'https://pro.ant.design/',
+                url: 'https://capstonecan.web.app/AboutUs',
                 openExternal: true,
               },
               {
                 title: 'Partners',
-                url: 'https://mobile.ant.design/',
+                url: 'https://capstonecan.web.app/FoodShift',
                 openExternal: true,
               },
             ],
@@ -76,121 +79,6 @@ export default function CustomFooter() {
      
       />
     </div>
+    </>
   );
-}
-
-//////
-
-// import '../../assets/index.less';
-// import React from 'react';
-// import Footer from 'rc-footer';
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Footer
-//         columns={[
-//           {
-//             title: '相关资源',
-//             items: [
-//               {
-//                 title: 'Ant Design Pro',
-//                 url: 'https://pro.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Ant Design Mobile',
-//                 url: 'https://mobile.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Kitchen',
-//                 url: 'https://kitchen.alipay.com/',
-//                 description: 'Sketch 工具集',
-//               },
-//             ],
-//           },
-//           {
-//             title: '社区',
-//             items: [
-//               {
-//                 title: 'Ant Design Pro',
-//                 url: 'https://pro.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Ant Design Mobile',
-//                 url: 'https://mobile.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Kitchen',
-//                 url: 'https://kitchen.alipay.com/',
-//                 description: 'Sketch 工具集',
-//               },
-//             ],
-//           },
-//           {
-//             title: '帮助',
-//             items: [
-//               {
-//                 title: 'Ant Design Pro',
-//                 url: 'https://pro.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Ant Design Mobile',
-//                 url: 'https://mobile.ant.design/',
-//                 openExternal: true,
-//               },
-//               {
-//                 title: 'Kitchen',
-//                 url: 'https://kitchen.alipay.com/',
-//                 description: 'Sketch 工具集',
-//               },
-//             ],
-//           },
-//           {
-//             icon: (
-//               <img
-//                 src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
-//                 alt="more products"
-//               />
-//             ),
-//             title: '更多产品',
-//             items: [
-//               {
-//                 icon: (
-//                   <img
-//                     src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg"
-//                     alt="yuque"
-//                   />
-//                 ),
-//                 title: '语雀',
-//                 url: 'https://yuque.com',
-//                 description: '知识创作与分享工具',
-//                 openExternal: true,
-//               },
-//               {
-//                 icon: (
-//                   <img
-//                     src="https://gw.alipayobjects.com/zos/rmsportal/uHocHZfNWZOdsRUonZNr.png"
-//                     alt="yuque"
-//                   />
-//                 ),
-//                 title: '云凤蝶',
-//                 url: 'https://yunfengdie.com',
-//                 description: '中台建站平台',
-//                 openExternal: true,
-//               },
-//             ],
-//           },
-//         ]}
-//         bottom="Made with ❤️ by AFX"
-          // bottom={element}
-        // theme="light"
-     // backgroundColor="#E07A5F"
-//       />
-//     </div>
-//   );
-// }
+      }
